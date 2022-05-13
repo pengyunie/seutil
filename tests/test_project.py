31d64afe8f079effab972430d754ef0919b39fb9
Pengyu Nie <prodigy.sov@gmail.com>
2022-05-13 14:17:04 -0500

    add a couple of more tests to project

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,7 @@
-from pathlib import Path
-import pytest
 import os
+from pathlib import Path
+
+import pytest
 import seutil as su
 
 resources_dir = Path(__file__).parent.parent / "test-subjects"
@@ -117,14 +118,99 @@ def test_clone_remote(remote_gitgame_repo: su.project.Project, tmp_path: Path):
     remote_gitgame_repo.clone(tmp_path)
     assert (tmp_path / "pengyunie_git-game").exists()
     assert (tmp_path / "pengyunie_git-game" / ".git").exists()
-    assert remote_gitgame_repo._dir == tmp_path / "pengyunie_git-game"
+    assert remote_gitgame_repo.dir == tmp_path / "pengyunie_git-game"
 
 
 # for the remaining tests about the operations after cloning, it does not matter if the project is from local or online
 
 
-def pptest_set_cloned_dir(local_gitgame_repo: su.project.Project, tmp_path: Path):
+def test_set_cloned_dir(local_gitgame_repo: su.project.Project, tmp_path: Path):
     local_gitgame_repo.clone(tmp_path)
     assert (tmp_path / "pengyunie_git-game").exists()
     assert (tmp_path / "pengyunie_git-game" / ".git").exists()
-    assert local_gitgame_repo._dir == tmp_path / "pengyunie_git-game"
+    assert local_gitgame_repo.dir == tmp_path / "pengyunie_git-game"
+
+    su.bash.run(
+        f"mv {tmp_path / 'pengyunie_git-game'} {tmp_path / 'pengyunie_git-game2'}", 0
+    )
+    assert local_gitgame_repo.dir == tmp_path / "pengyunie_git-game"
+
+    local_gitgame_repo.set_cloned_dir(tmp_path / "pengyunie_git-game2")
+    assert local_gitgame_repo.dir == tmp_path / "pengyunie_git-game2"
+
+
+def test_remove(local_gitgame_repo: su.project.Project, tmp_path: Path):
+    local_gitgame_repo.clone(tmp_path)
+    assert local_gitgame_repo.dir == tmp_path / "pengyunie_git-game"
+
+    local_gitgame_repo.remove()
+    assert not (tmp_path / "pengyunie_git-game").exists()
+    with pytest.raises(RuntimeError):
+        local_gitgame_repo.dir
+
+
+def test_fetch(local_gitgame_repo: su.project.Project, tmp_path: Path):
+    local_gitgame_repo.clone(tmp_path)
+    assert (tmp_path / "pengyunie_git-game").exists()
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "d851edda3009332dd5d3f8f949a102f279dad809"
+    )
+    # undo the last commit, to test if it is later pulled back
+    with su.io.cd(local_gitgame_repo.dir):
+        su.bash.run("git reset --hard HEAD~1", 0)
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30"
+    )
+
+    local_gitgame_repo.fetch()
+    with su.io.cd(local_gitgame_repo.dir):
+        su.bash.run("git checkout origin/master", 0)
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "d851edda3009332dd5d3f8f949a102f279dad809"
+    )
+
+
+def test_checkout(local_gitgame_repo: su.project.Project, tmp_path: Path):
+    local_gitgame_repo.clone(tmp_path)
+    assert (tmp_path / "pengyunie_git-game").exists()
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "d851edda3009332dd5d3f8f949a102f279dad809"
+    )
+    local_gitgame_repo.checkout("7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30")
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30"
+    )
+
+
+def test_checkout_forced(local_gitgame_repo: su.project.Project, tmp_path: Path):
+    local_gitgame_repo.clone(tmp_path)
+    assert (tmp_path / "pengyunie_git-game").exists()
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "d851edda3009332dd5d3f8f949a102f279dad809"
+    )
+    # modify the README so that normal checkout should fail
+    su.io.dump(local_gitgame_repo.dir / "README.md", "modified", su.io.Fmt.txt)
+    with pytest.raises(su.bash.BashError):
+        local_gitgame_repo.checkout("7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30")
+
+    # a forced checkout should do it
+    local_gitgame_repo.checkout("7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30", forced=True)
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "7c8c3ccc2f4bb118a657f1f7a7ab4e163d1b7a30"
+    )
+
+
+def test_get_cur_revision(local_gitgame_repo: su.project.Project, tmp_path: Path):
+    local_gitgame_repo.clone(tmp_path)
+    assert (tmp_path / "pengyunie_git-game").exists()
+    assert (
+        local_gitgame_repo.get_cur_revision()
+        == "d851edda3009332dd5d3f8f949a102f279dad809"
+    )
