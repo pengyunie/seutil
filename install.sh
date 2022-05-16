53594af5ff145d58c24f598c98f27d3b4278c88f
Pengyu Nie <prodigy.sov@gmail.com>
2022-05-16 15:29:16 -0500

    Clean up deprecated scripts.

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$(python --version 2>&1)" == "Python 3.7"* ]]; then
-        pip install --upgrade seutil
-else
-        echo "Requires Python 3.7!" 1>&2
-        echo "You can install anaconda 3 to use Python 3.7:" 1>&2
-        echo "$ ./install_anaconda.sh" 1>&2
-fi
