0069297bd9cbc022d645058c4236c95b2e4f05f3
Pengyu Nie <prodigy.sov@gmail.com>
2022-09-09 15:38:17 -0500

    update __init__ with some tricks for lazy_import

--- seutil/__init__.py
+++ seutil/__init__.py
@@ -7,7 +7,7 @@ if module_root not in sys.path:
 
 import lazy_import
 
-from . import arg, bash, io, log, pbar, project
+from . import arg, bash, io, log, pbar
 from .BashUtils import BashUtils
 from .GitHubUtils import GitHubUtils
 from .IOUtils import IOUtils
@@ -15,22 +15,28 @@ from .LoggingUtils import LoggingUtils
 from .Stream import Stream
 from .TimeUtils import TimeoutException, TimeUtils
 
-
+project = lazy_import.lazy_module("seutil.project")
+latex = lazy_import.lazy_module("seutil.latex")
 ds = lazy_import.lazy_module("seutil.ds")
 
+# tricks the IDE to recognize the lazy imports, so that it can provide code completion
+# won't be executed
+if 1.0 == 1.01:
+    from . import ds, latex, project
+
+
 __all__ = [
-    # Classes
-    "BashUtils",
-    "CliUtils",
+    "arg",
+    "bash",
+    "io",
+    "log",
+    "latex",
+    "project",
+    "ds",
     "GitHubUtils",
-    "IOUtils",
-    "LoggingUtils",
     "MiscUtils",
     "Stream",
     "TimeUtils",
-    # Sub-Packages
-    "latex",
-    # Exceptions
     "TimeoutException",
 ]
 
