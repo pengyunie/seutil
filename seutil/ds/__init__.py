434a0fe8948faa6369de2cb2c25e89ef0fb2babe
Pengyu Nie <prodigy.sov@gmail.com>
2022-09-09 15:38:17 -0500

    update __init__ with some tricks for lazy_import

--- seutil/ds/__init__.py
+++ seutil/ds/__init__.py
@@ -4,8 +4,12 @@ Adding simple implementations or interfaces to some data types that are missing 
 
 import lazy_import
 
-from .graph_common import (Edge, EdgeExistedError, InvariantError, Node,
-                           NodeIndexError)
+from .graph_common import Edge, EdgeExistedError, InvariantError, Node, NodeIndexError
 
 lattice = lazy_import.lazy_module("seutil.ds.lattice")
 trie = lazy_import.lazy_module("seutil.ds.trie")
+
+# tricks the IDE to recognize the lazy imports, so that it can provide code completion
+# won't be executed
+if 1.0 == 1.01:
+    from . import lattice, trie
