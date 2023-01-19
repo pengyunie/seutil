7d88db33fd7521eea13dc056151f263fa966c16f
Pengyu Nie <prodigy.sov@gmail.com>
2023-01-19 11:59:23 -0600

    add latex.escape

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -8,3 +8,16 @@ def test_file_auto_notice(tmp_path: Path):
     assert su.io.load(f.path, su.io.Fmt.txt).endswith(
         "test_latex.py test_file_auto_notice\n"
     )
+
+
+def test_escape():
+    assert su.latex.escape("a") == "a"
+    assert su.latex.escape("a & b") == r"a \& b"
+    assert su.latex.escape("42%") == r"42\%"
+    assert su.latex.escape("$42") == r"\$42"
+    assert su.latex.escape("#x") == r"\#x"
+    assert su.latex.escape("a_b") == r"a\_b"
+    assert su.latex.escape("a{b}") == r"a\{b\}"
+    assert su.latex.escape("a~") == r"a\textasciitilde{}"
+    assert su.latex.escape("a^") == r"a\textasciicircum{}"
+    assert su.latex.escape("a\\") == r"a\textbackslash{}"
