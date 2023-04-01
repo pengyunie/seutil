798cc36b4aff163c32d3cf36562c1aae7cd1718a
Pengyu Nie <prodigy.sov@gmail.com>
2023-03-31 23:42:20 -0500

    migrate test_bash to pytest style

--- tests/test_bash.py
+++ tests/test_bash.py
@@ -1,37 +1,36 @@
 import os
-import unittest
+
+import pytest
 
 import seutil as su
 
 
-class test_bash_run(unittest.TestCase):
-    def test_run(self):
-        self.assertEqual("hello world\n", su.bash.run("echo 'hello world'").stdout)
+def test_run():
+    assert su.bash.run("echo 'hello world'").stdout == "hello world\n"
 
-    def test_run_fail(self):
-        self.assertRaises(
-            su.bash.BashError,
-            su.bash.run,
-            "echo 'hello world' && exit 1",
-            check_returncode=0,
-        )
-        self.assertRaises(
-            su.bash.BashError, su.bash.run, "echo 'hello world'", check_returncode=1
-        )
 
-    def test_inherit_env(self):
-        os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
-        self.assertEqual("1\n", su.bash.run("echo $TEST_SEUTIL_BASH_ENV").stdout)
+def test_check_returncode():
+    with pytest.raises(su.bash.BashError):
+        su.bash.run("echo 'hello world' && exit 1", check_returncode=0)
 
-    def test_update_env(self):
-        os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
-        su.bash.run("export TEST_SEUTIL_BASH_ENV=2", update_env=True)
-        self.assertEqual("2", os.environ["TEST_SEUTIL_BASH_ENV"])
+    with pytest.raises(su.bash.BashError):
+        su.bash.run("echo 'hello world'", check_returncode=1)
 
-        os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
-        su.bash.run(
-            "unset TEST_SEUTIL_BASH_ENV",
-            update_env=True,
-            update_env_clear_existing=True,
-        )
-        self.assertTrue("TEST_SEUTIL_BASH_ENV" not in os.environ)
+
+def test_inherit_env():
+    os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
+    assert su.bash.run("echo $TEST_SEUTIL_BASH_ENV").stdout == "1\n"
+
+
+def test_update_env():
+    os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
+    su.bash.run("export TEST_SEUTIL_BASH_ENV=2", update_env=True)
+    assert os.environ["TEST_SEUTIL_BASH_ENV"] == "2"
+
+    os.environ["TEST_SEUTIL_BASH_ENV"] = "1"
+    su.bash.run(
+        "unset TEST_SEUTIL_BASH_ENV",
+        update_env=True,
+        update_env_clear_existing=True,
+    )
+    assert "TEST_SEUTIL_BASH_ENV" not in os.environ
