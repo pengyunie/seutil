6cfab3a7f1d3e12f7f862285ce456d4f96135cf2
Pengyu Nie <prodigy.sov@gmail.com>
2023-05-10 16:27:00 -0500

    [build] update the initialization of conda env in prepare-env.sh; different approach for python-build without building docker

--- prepare-env.sh
+++ prepare-env.sh
@@ -3,33 +3,17 @@
 
 _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-DEFAULT_CONDA_PATH="$HOME/opt/anaconda3/etc/profile.d/conda.sh"
-
-
-function get_conda_path() {
-        local conda_exe=$(which conda)
-        if [[ -z ${conda_exe} ]]; then
-                echo "Fail to detect conda! Have you installed Anaconda/Miniconda?" 1>&2
-                exit 1
-        fi
-
-        echo "$(dirname ${conda_exe})/../etc/profile.d/conda.sh"
-}
-
-
 function prepare_conda_env() {
         # the python version to use
         local python_version=${1:-3.8}; shift
         # the conda env name
         local env_name=${1:-seutil}; shift
-        # path to conda.sh script; automatically inferred from conda executable path if not provided
-        local conda_path=${1:-$(get_conda_path)}; shift
 
-        echo ">>> Preparing conda environment \"${env_name}\", python_version=${python_version}, conda=${conda_path}"
+        echo ">>> Preparing conda environment \"${env_name}\", python_version=${python_version}"
         
         # Preparation
         set -e
-        source ${conda_path}
+        eval "$(conda shell.bash hook)"
         conda env remove --name $env_name
         conda create --name $env_name python=$python_version pip -y
         conda activate $env_name
