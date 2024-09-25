faece6e1081c3879d9082c9f446a37279bfe6192
Pengyu Nie <prodigy.sov@gmail.com>
2024-09-25 00:41:05 -0400

    update CI workflows - merge prepare-env-no-opt-deps.sh into prepare-env.sh, with a "false" 3rd argument - include setup-miniconda in testing workflows - update the actions' versions in general

--- prepare-env.sh
+++ prepare-env.sh
@@ -8,20 +8,26 @@ function prepare_conda_env() {
         local python_version=${1:-3.10}; shift
         # the conda env name
         local env_name=${1:-seutil}; shift
+        # if true, install optional dependencies
+        local opt_deps=${1:-true}; shift
 
         echo ">>> Preparing conda environment \"${env_name}\", python_version=${python_version}"
         
         # Preparation
         set -e
         eval "$(conda shell.bash hook)"
-        conda env remove --name $env_name
+        conda env remove --name $env_name -y
         conda create --name $env_name python=$python_version pip -y
         conda activate $env_name
         pip install --upgrade pip
 
         # Install libraries
-        pip install torch --index-url https://download.pytorch.org/whl/cpu  # prioritize the use of cpu version of pytorch
-        pip install -e .[dev,dev-opt]
+        if [ "$opt_deps" = true ]; then
+                pip install torch --index-url https://download.pytorch.org/whl/cpu  # prioritize the use of cpu version of pytorch
+                pip install -e .[dev,dev-opt]
+        else
+                pip install -e .[dev]
+        fi
 }
 
 
